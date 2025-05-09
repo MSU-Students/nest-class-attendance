b592976ad8e093a10a838a6a4d13294c31933dbb
Mohaymen Omar <mohaymenomar13@gmail.com>
2025-05-09 09:08:45 +0800

    Update roles.module.ts
    Fixed an error by importing the User entity

--- src/users/roles.module.ts
+++ src/users/roles.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Role } from './entities/role.entity';
+import { User } from './entities/user.entity';
 import { RolesService } from './roles.service';
 import { RolesController } from './roles.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Role])],
+  imports: [TypeOrmModule.forFeature([Role, User])],
   providers: [RolesService],
   controllers: [RolesController],
   exports: [RolesService], // Optional: only if you want to use it in other modules
